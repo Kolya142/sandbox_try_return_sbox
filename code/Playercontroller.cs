8d339fce4f4521d453ba548b931e4c7f1a4478a6
Kolya <87275865+Kolya142@users.noreply.github.com>
2024-03-14 20:42:45 +0300

    too many changes

--- code/Playercontroller.cs
+++ code/Playercontroller.cs
@@ -7,11 +7,13 @@ public sealed class Playercontroller : Component
 	Angles angles = new Angles();
 	[Property] public GameObject aim_show;
 	[Property] public GameObject point;
+	bool CanDrag;
+	Vector3 lastobjectPos;
 	GameObject moveObject = null;
 	float distObject;
 	BBox hull = new BBox(
-		new Vector3( -1 ),
-		new Vector3( 1 )
+		new Vector3( -10 ),
+		new Vector3( 10 )
 	);
 	protected override void OnAwake()
 	{
@@ -19,7 +21,10 @@ public sealed class Playercontroller : Component
 	}
 	private SceneTraceResult Trace(Vector3 start, Vector3 end)
 	{
-		return Scene.Trace.Ray(end, start).Size(hull).Run();
+		return Scene.Trace.Ray(start, end )
+			.UsePhysicsWorld()
+			.Radius(2f)
+			.Run();
 	}
 	protected override void OnUpdate()
 	{
@@ -35,18 +40,19 @@ public sealed class Playercontroller : Component
 							MathF.Pow( aim.EndPosition.z - Transform.Position.z, 2 )
 							);
 		GameObject picker = aim.GameObject;
-		if ( Input.Pressed( "attack2" ) )
+		if ( Input.Pressed( "attack2" ) && !Input.Down( "attack1" ) )
 		{
 			GameObject newobject = new GameObject( true, "spawned" );
 			newobject.Transform.Position = aim.HitPosition;
 			newobject.Components.Create<ModelRenderer>();
 			newobject.Components.GetInChildrenOrSelf<ModelRenderer>().Model = Model.Cube;
 			newobject.Components.Create<BoxCollider>();
-			// newobject.Components.Create<Rigidbody>();
+			newobject.Components.Create<Rigidbody>();
 		}
+		// Log.Info(picker);
 		if ( picker != null )
 		{
-			if ( Input.Down( "attack1" ) )
+			if ( Input.Down( "attack1" ) && CanDrag )
 			{
 				if ( moveObject == null )
 				{
@@ -54,6 +60,7 @@ public sealed class Playercontroller : Component
 					{
 						moveObject = picker;
 						distObject = dist;
+						lastobjectPos = picker.Transform.Position;
 						Rigidbody moveBody = moveObject.Components.GetInChildrenOrSelf<Rigidbody>();
 						if ( moveBody != null )
 							moveBody.MotionEnabled = false;
@@ -61,21 +68,38 @@ public sealed class Playercontroller : Component
 				}
 				else
 				{
+					Rigidbody moveBody = moveObject.Components.GetInChildrenOrSelf<Rigidbody>();
 					moveObject.Transform.Position = Transform.Position + Transform.Rotation.Forward * distObject;
+					if ( moveBody != null )
+					{
+						Vector3 velocity = moveBody.Velocity;
+						Vector3.SmoothDamp( lastobjectPos, moveObject.Transform.Position, ref velocity, 0.075f, Time.Delta );
+						moveBody.Velocity = velocity;
+						if ( Input.Pressed( "attack2" ) )
+						{
+							CanDrag = false;
+						}
+					}
+					lastobjectPos = picker.Transform.Position;
 				}
 			}
-			else
+			if ( !Input.Down( "attack1" ) )
 			{
-				if (moveObject != null)
+				if ( moveObject != null && CanDrag )
 				{
-					moveObject.Components.GetInChildrenOrSelf<Rigidbody>().MotionEnabled = true;
+					Rigidbody moveBody = moveObject.Components.GetInChildrenOrSelf<Rigidbody>();
+					if ( moveBody != null )
+					{
+						moveBody.MotionEnabled = true;
+					}
 				}
+				CanDrag = true;
 				moveObject = null;
 			}
 		}
 		Vector3 move = Input.AnalogMove;
 		if ( Input.Down( "Run" ) )
-			move *= 2;
+			move *= 5;
 		// Log.Info( move );
 		//Transform.Position += Transform.Rotation.ClosestAxis(move);
 		// /*
