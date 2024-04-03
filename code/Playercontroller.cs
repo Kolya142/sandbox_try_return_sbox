e1b092f8d730650d35ad63731df43df14c1ce84e
Kolya <87275865+Kolya142@users.noreply.github.com>
2024-04-03 15:45:54 +0300

    rope(ragdoll), dublicator name

--- code/Playercontroller.cs
+++ code/Playercontroller.cs
@@ -43,7 +43,7 @@ public sealed class Playercontroller : Component
 	int ind = 0;
 	bool cnasd = true;
 	public List<SceneParticles> particles = new List<SceneParticles>();
-	string[] Tools = ["PhysGun", "Spawner", "Gun", "Scale", "GravGun", "Thruster", "Remove", "Color", "Balloon", "Light", "Display", "Info", "Save", "Rope", "Weld", "Lidar", "CreateTerry"];
+	string[] Tools = ["PhysGun", "Spawner", "Dublicator", "Gun", "Scale", "GravGun", "Thruster", "Remove", "Color", "Balloon", "Light", "Display", "Info", "Save", "Rope", "Weld", "Lidar", "CreateTerry"];
 	public Dictionary<GameObject, Color> DefaultColors = new();
 
 	public static Playercontroller Local => GameManager.ActiveScene.Components.GetAll<Playercontroller>( FindMode.EnabledInSelfAndDescendants ).ToList().FirstOrDefault( x => x.Network.OwnerConnection.SteamId == (ulong)Game.SteamId );
---
+++ code/RagDollRope.cs
@@ -0,0 +1,36 @@
+﻿using Sandbox;
+
+public sealed class RagdollRope : Component
+{
+	[Property] public PhysicsBody physics;
+	[Property] public GameObject Object;
+	GameObject joint;
+	bool init = false;
+	protected override void OnUpdate()
+	{
+		if ( physics == null || Object == null || !Object.IsValid || !physics.IsValid() )
+			return;
+		if ( !init )
+		{
+			joint = new GameObject();
+			var joints = joint.Components.Create<SpringJoint>();
+			joints.Body = Object;
+			joints.Frequency = 5;
+			joints.Damping = 0.7f;
+			joints.EnableCollision = false;
+			joints.MinLength = 10f;
+
+			joints = Object.Components.Create<SpringJoint>();
+			joints.Body = joint;
+			joints.Frequency = 5;
+			joints.Damping = 0.7f;
+			joints.EnableCollision = false;
+			joint.Components.Create<Rigidbody>();
+			init = true;
+		}
+		if ( joint != null && joint.Transform != null )
+		{
+			physics.SmoothMove( joint.Transform.Position, 0.1f, Time.Delta );
+		}
+	}
+}
